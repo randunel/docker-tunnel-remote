8b8fe655ef09c99c8efd7c58239123b6ab3d7509
Mihai Ene <dkrandu@yahoo.com>
2016-08-04 17:09:09 +0100

    Use openvpn.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM ubuntu:16.04
+FROM alpine:3.4
 
-RUN apt-get update && \
-        apt-get install -y iptables iproute2 unbound
-
-# RUN apk add --no-cache unbound bash && \
-#         rm -rf /var/cache/apk/*
+RUN apk add --no-cache \
+        bash \
+        iptables \
+        iproute2 \
+        openvpn \
+        unbound \
+        && rm -rf /var/cache/apk/*
 
 ADD unbound.conf /etc/unbound/unbound.conf
 ADD entrypoint.sh /root/entrypoint.sh
 
-ENTRYPOINT ["/bin/bash"]
-CMD ["/root/entrypoint.sh"]
+ENTRYPOINT ["/root/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-trap "printf '\nBye.\n'; exit 0;" SIGHUP SIGINT SIGTERM
+forward_all_signals_to() {
+    SIGNALS=(SIGHUP SIGINT SIGTERM SIGQUIT SIGSTOP SIGUSR1 SIGUSR2);
+    for SIGNAL in ${SIGNALS[@]}; do
+        eval "trap \"printf \\\"\\n$0 forwarding signal $SIGNAL to pid $1.\\n\\\"; kill -$SIGNAL $1;\" \"$SIGNAL\";"
+    done;
+}
 
 confirm_env_exists() {
     local NAME=$1;
@@ -9,27 +14,64 @@ confirm_env_exists() {
         exit 1;
     fi
 }
-for env in DOCKER_NETWORK DEV_IP OWN_IP; do
+
+continue_init() {
+    printf "Continue init\n";
+
+    trap - SIGUSR2;
+    trap - SIGINT;
+
+    unbound &
+
+    printf "Executing openvpn.\n";
+    exec 3< <(openvpn "${@:1}");
+    OPENVPN_PID=$!;
+    forward_all_signals_to $OPENVPN_PID;
+
+    printf "Reading pid $OPENVPN_PID.\n";
+
+    while read line; do
+        case $line in
+            *"Initialization Sequence Completed"*)
+                printf "Openvpn started.\n";
+                break;
+                ;;
+            *)
+                printf " $line\n";
+        esac
+    done <&3;
+
+    # exec 3<&-;
+
+    printf "Setting up iptables\n";
+    iptables -t nat -A POSTROUTING -o $DOCKER_IF -j MASQUERADE;
+    iptables -t nat -A POSTROUTING -o $OPENVPN_IF -j MASQUERADE;
+
+    printf "Setting up ip route\n";
+    ip r a $DOCKER_NETWORK dev $DOCKER_IF;
+
+    tail -f /dev/null &
+    TAIL_PID=$!;
+    trap "kill -9 $TAIL_PID; printf \"got SIGINT\"; exit 0;" SIGINT;
+    wait;
+}
+
+for env in DOCKER_NETWORK; do
     confirm_env_exists $env;
 done;
 
-GRE_NETWORK="10.160.2.0/24";
-GRE_IP="10.160.2.1/24";
+if [ -z "$DOCKER_IF" ]; then
+    DOCKER_IF="eth1";
+fi
 
-printf "Setting up GRE tunnel\n";
-ip tunnel add gre0 mode gre remote $DEV_IP local $OWN_IP ttl 255;
-ip link set gre0 up;
-ip addr add $GRE_IP dev gre0;
+if [ -z "$OPENVPN_IF" ]; then
+    OPENVPN_IF="tun0";
+fi
 
-printf "Starting unbound daemon\n";
-unbound &
-UNBOUND_PID=$!;
+printf "Waiting for SIGUSR2.\n";
 
-printf "Setting up iptables\n";
-iptables -t nat -A POSTROUTING -j MASQUERADE;
-
-printf "Setting up ip route\n";
-ip r a $GRE_NETWORK dev gre0;
-
-printf "Waiting for daemon pid $UNBOUND_PID.\n";
-wait $UNBOUND_PID;
+tail -f /dev/null &
+TAIL_PID=$!;
+trap "kill -9 $TAIL_PID; printf \"got SIGUSR2\"; continue_init \"\$@\";" SIGUSR2;
+trap "kill -9 $TAIL_PID; printf \"got SIGINT\"; exit 1;" SIGINT;
+wait;
